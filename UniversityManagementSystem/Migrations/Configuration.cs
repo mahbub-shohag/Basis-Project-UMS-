9d8dc15ec0691cfb1f98895dbe1337541aadc9a9
mahbub-shohag <shohag.cse3@gmail.com>
2014-12-29 01:16:06 +0600

    presentation day was 22 december 2014

---
+++ UniversityManagementSystem/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace UniversityManagementSystem.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<UniversityManagementSystem.DAL.UniversityDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "UniversityManagementSystem.DAL.UniversityDbContext";
+        }
+
+        protected override void Seed(UniversityManagementSystem.DAL.UniversityDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
